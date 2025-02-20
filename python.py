0056a55859066927eda80b810bc72eb59782aee2
iareARiES <adityapstakuli@gmail.com>
2025-02-21 02:24:47 +0530

    Commit

--- python.py
+++ python.py
@@ -1,3 +1,13 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+dataset = pd.read_csv("breast_cancer.csv")
+
+print(dataset.head())
+
+X = dataset.drop(columns =["Class"])
+y = dataset["Class"]
+
+
